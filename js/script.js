e8f65ae54a7bd7d2eddf70084b091a35fbf6c2eb
B7rawy <kareemelb7rawy@gmail.com>
2023-04-02 08:59:05 +0200

    first commet

---
+++ js/script.js
@@ -0,0 +1,74 @@
+let menu = document.querySelector('#menu-bars');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+}
+
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header .navbar a');
+
+window.onscroll = () =>{
+  document.addEventListener('click', (e) => {
+    if (!navbar.contains(e.target) && !menu.contains(e.target)) {
+      menu.classList.remove('fa-times');
+      navbar.classList.remove('active');
+    }
+  });
+  
+  /*
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+  */
+  section.forEach(sec =>{
+
+    let top = window.scrollY;
+    let height = sec.offsetHeight;
+    let offset = sec.offsetTop - 150;
+    let id = sec.getAttribute('id');
+
+    if(top >= offset && top < offset + height){
+      navLinks.forEach(links =>{
+        links.classList.remove('active');
+        document.querySelector('header .navbar a[href*='+id+']').classList.add('active');
+      });
+    };
+
+  });
+
+}
+
+
+document.querySelector('#search-icon').onclick = () =>{
+  document.querySelector('#search-form').classList.toggle('active');
+}
+
+document.querySelector('#close').onclick = () =>{
+  document.querySelector('#search-form').classList.remove('active');
+};
+
+
+
+ 
+
+function loader(){
+  document.querySelector('.loader-container').classList.add('fade-out');
+}
+
+function fadeOut(){
+  setInterval(loader, 1000);
+}
+
+window.onload = fadeOut;
+
+
+
+
+
+
+
+
+
+
+
